6c83e1dc2a988015af3f4a5ce14c73d2c84eaecc
Saad <saad.saif@phpstudios.com>
2022-05-22 00:52:37 +0500

    class based views

--- authentication/views.py
+++ authentication/views.py
@@ -1,17 +1,23 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
+from django.views.generic import View
 
 from authentication import forms
 
-def logout_user(request):
-  logout(request)
-  return redirect('login')
+class LoginPage(View):
 
-def login_page(request):
-  message = ''
-  form = forms.LoginFrom()
-  if request.method == 'POST':
-    form = forms.LoginFrom(request.POST)
+  form_class = forms.LoginFrom
+  template_name = 'authentication/login.html'
+
+  def get(self, request):
+    message = ''
+    return render(request, self.template_name, context={
+              'form': self.form_class(),
+              'message': message
+            })
+
+  def post(self, request):
+    form = self.form_class(request.POST)
     if form.is_valid():
       user = authenticate(
         username=form.cleaned_data['username'], 
@@ -22,7 +28,12 @@ def login_page(request):
         return redirect('home')
       else:
         message = f"Login failed!"
-  return render(request, 'authentication/login.html', context={
-    'form': form,
-    'message': message
-  })
+    return render(request, self.template_name, context={
+              'form': form,
+              'message': message
+            })
+
+
+def logout_user(request):
+  logout(request)
+  return redirect('login')
